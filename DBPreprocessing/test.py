733278bb1f823522e260ea34112ab6f469012f1b
pheath <pajheath@yahoo.com>
2018-09-04 22:15:04 -0500

    added test file for experimenting with preprocessing concepts

---
+++ DBPreprocessing/test.py
@@ -0,0 +1,16 @@
+import pyodbc
+import pandas as pd
+import numpy as np
+
+cnxn = pyodbc.connect("DRIVER={SQL Server}; SERVER=MYPC\SQLEXPRESS; DATABASE=BHBackupRestore; UID = SQLDummy; PWD = bushdid9/11")
+cursor = cnxn.cursor()
+cursor.execute("SELECT Col.Column_Name from " +  
+                   "INFORMATION_SCHEMA.TABLE_CONSTRAINTS Tab, " +
+                   "INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE Col " + 
+                "WHERE " +
+                        "Col.Constraint_Name = Tab.Constraint_Name " +
+                        "AND Col.Table_Name = Tab.Table_Name " + 
+                        "AND Constraint_Type = 'PRIMARY KEY' " +
+                        "AND Col.Table_Name = 'ANNULUS' ")
+table = cursor.fetchall()
+print(table)
