92d782798bdf8fb8e44e04ffe3598f8c539bb3b3
pheath <pajheath@yahoo.com>
2018-09-06 14:48:41 -0500

    initial rewriting to accomidate memory-saving design

--- DBPreprocessing/DBPMain.py
+++ DBPreprocessing/DBPMain.py
@@ -28,58 +28,6 @@ def getTableNames():
         tableNames.append(row[0])
     return tableNames
 
-def getFrames(tableNames, cursor):
-    frames = {}
-    for name in tableNames:
-        cursor.execute('SELECT * FROM [dbo].[' + name + ']');
-        table = cursor.fetchall()
-        columns = [column[0] for column in cursor.description]
-        dictArr = []
-        for row in table:
-            entry = {}
-            for col in columns:
-                entry[columns.index(col)] = row[columns.index(col)]
-            dictArr.append(entry)
-        frame = pd.DataFrame(dictArr)
-        if len(frame.columns) == 0:
-            for col in columns:
-                frame[col] = [None]
-        else:
-            frame.columns = columns
-        frames[name] = frame
-    return frames
-
-def getKeys(tableNames, cursor):
-    keys = {}
-    for name in tableNames:
-        cursor.execute("SELECT Col.Column_Name from " +  
-                           "INFORMATION_SCHEMA.TABLE_CONSTRAINTS Tab, " +
-                           "INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE Col " + 
-                       "WHERE " +
-                           "Col.Constraint_Name = Tab.Constraint_Name " +
-                           "AND Col.Table_Name = Tab.Table_Name " + 
-                           "AND Constraint_Type = 'PRIMARY KEY' " +
-                           "AND Col.Table_Name = '" + name + "'")
-        table = cursor.fetchall()
-        tableKeys = []
-        for key in table:
-            tableKeys.append(key[0])
-        keys[name] = tableKeys
-    return keys
-
-def getFeatures(tableNames, cursor):
-    features = {}
-    for name in tableNames:
-        cursor.execute("SELECT COLUMN_NAME " +
-                       "FROM INFORMATION_SCHEMA.COLUMNS " +
-                       "WHERE TABLE_NAME = '" + name + "'")
-        table = cursor.fetchall()
-        tableFeatures = []
-        for feature in table:
-            tableFeatures.append(feature[0])
-        features[name] = tableFeatures
-    return features
-
 def getTableInfo(tableNames, cursor, sqlCode):
     infoListByName = {}
     for name in tableNames:
@@ -90,7 +38,6 @@ def getTableInfo(tableNames, cursor, sqlCode):
             infoInList.append(k[0])
         infoListByName[name] = infoInList
     return infoListByName
-    
 
 ########## main ##########
 if __name__ == "__main__":
@@ -98,5 +45,3 @@ if __name__ == "__main__":
     tableNames = getTableNames()
     PKs = getTableInfo(tableNames, cursor, GETPRIMARYKEYSQLCODE)
     features = getTableInfo(tableNames, cursor, GETFEATURESSQLCODE)
-    print(PKs)
-    print(features)
