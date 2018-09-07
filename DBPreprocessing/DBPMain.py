f98f9fb15d6fd5d92ae3a6467187826f1943cce1
pheath <pajheath@yahoo.com>
2018-09-06 22:30:33 -0500

    added not-debugged code to create paths - we'll see if all paths take too much memory

--- DBPreprocessing/DBPMain.py
+++ DBPreprocessing/DBPMain.py
@@ -39,9 +39,61 @@ def getTableInfo(tableNames, cursor, sqlCode):
         infoListByName[name] = infoInList
     return infoListByName
 
+def findParentTables(tableName, tablePK, features):
+    parentNames = []
+    for name, featureList in features.items():
+        if (tableName != name) and keyContains(tablePK, featureList):
+            parentNames.append(name)
+    return parentNames
+
+def keyContains(key, featureList):
+    matches = 0
+    for k in key:
+        for feature in featureList:
+            if k == feature:
+                matches += 1
+    return (matches == len(key))
+
+#returns dictionary indexed by table name with lists of paths from table to terminal parent 
+def createPaths(tableNames, PKs, features):
+    paths = {}
+    for name in tableNames:
+        pathGroup = []
+        paths[name] = pathGroup
+        #@@@@@@@@GIVES KEY ERROR CHECK THAT ORIGINAL TABLE IS NOT TERMINAL TABLE@@@@@@@@
+        directParents = findParentTables(name, PKs[name], features)
+        for parent in directParents:
+            path = []
+            path.append(parent)
+            pathGroup.append(path)   
+        pathsCompleted = False
+        while not pathsCompleted:
+            pathsToAdd = []
+            pathToRemove = []
+            for path in pathGroup:
+                nextParents = findParentTables(path[len(path) - 1], PKs[len(path) - 1], features)
+                if len(nextParents) != 0:
+                    pathToRemove = path.copy()
+                    for parent in nextParents:
+                        pathsToAdd.apend(path.copy().append(parent))
+                    break
+            if (len(pathsToAdd) == 0) and (len(pathToRemove) == 0):
+                pathsCompleted = True
+            else:
+                pathGroup.remove(pathToRemove)
+                for path in pathsToAdd:
+                    pathGroup.append(path)
+        paths[name] = pathGroup
+    return paths
+
 ########## main ##########
 if __name__ == "__main__":
     cursor = connect()
     tableNames = getTableNames()
     PKs = getTableInfo(tableNames, cursor, GETPRIMARYKEYSQLCODE)
     features = getTableInfo(tableNames, cursor, GETFEATURESSQLCODE)
+    print(createPaths(tableNames, PKs, features))
+    
+#    for name in tableNames:
+#        print(findParentTables(name, PKs[name], features))
+#        print()
