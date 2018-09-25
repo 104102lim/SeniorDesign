90ebd29a44f2091f77b26efde8f4839fb6834532
pheath <pajheath@yahoo.com>
2018-09-25 15:25:11 -0500

    gets list of all valid table descriptions now

--- DBPreprocessing/DBPMain.py
+++ DBPreprocessing/DBPMain.py
@@ -1,6 +1,7 @@
 import pyodbc
 import pandas as pd
-import numpy as np
+import os
+import EnumerateDescriptions as ED
 
 GETPRIMARYKEYSQLCODE = "SELECT Col.Column_Name from " +  \
                            "INFORMATION_SCHEMA.TABLE_CONSTRAINTS Tab, " +  \
@@ -16,6 +17,56 @@ GETFOREIGNKEYSQLCODE = GETPRIMARYKEYSQLCODE.replace("PRIMARY KEY", "FOREIGN KEY"
 GETFEATURESSQLCODE = "SELECT COLUMN_NAME " +  \
                      "FROM INFORMATION_SCHEMA.COLUMNS " +  \
                      "WHERE TABLE_NAME = '"
+
+class Tree:
+    def __init__(self, root):
+        self.root = root
+
+    def contains(self, node):
+        return self.containsHelper(node, self.root)
+
+    def containsHelper(self, node1, node2):
+        if node1.name == node2.name:
+            return True
+        else:
+            ret = False
+            for c in node2.children:
+                ret = ret or self.containsHelper(node1, c)
+
+    def getLeaves(self):
+        return self.getLeavesHelper(self, self.root)
+
+    def getLeavesHelper(self, node):
+        if len(node.children) == 0:
+            return [node]
+        else:
+            ret = []
+            for c in node.children:
+                ret += self.getLeavesHelper(c)
+            return ret
+
+    def __str__(self):
+        return
+
+    def findCommonAncestor(self, nodes):
+        return
+
+    def getAncestorTree(self, nodes):
+        return
+        #use findCommonAncestor
+
+    def writeSQL(self, ti):
+        return
+
+class Node:
+    def __init__(self, name, children, parent):
+        self.children = children
+        self.name = name
+        self.parent = parent
+
+    def __str__(self):
+        return
+
 
 #connect to sql server
 def connect():
@@ -79,59 +130,102 @@ def getRootParents(ti):
             roots.append(name)
     return roots
 
-def createComd(root, ti):
-    included = []
-    included.append(root)
-    cmd = "SELECT * FROM " + root
-    children = {}
-    children[root] = getChildren(root, ti)
-    numChildren = len(children[root])
-    while numChildren > 0:    # No no no lol
-        newChildren = {}
-        for p in children:
-            for c in children[p]:
-                if c not in included:
-                    included.append(c)
-                    newChildren[c] = getChildren(c, ti)
-                    # Potential problem with multi column keys
-                    cmd += " FULL JOIN " + c + " ON " + p + "." + ti[1][c][0] + \
-                           "=" + c + "." + ti[1][c][0]
-        children = newChildren
-        numChildren = 0
-        for p in children:
-            numChildren += len(children[p])
-    return cmd
+def makeTree(rt, ti):
+    tree = Tree(Node(rt, [], None))
+    leaves = tree.getLeaves()
+    while True:
+        for l in leaves:
+            children = getChildren(l.name, ti)
+            for c in children:
+                child = Node(c, [], l)
+                if not tree.contains(child):
+                    l.children.append(child)
+        newLeaves = tree.getLeaves()
+        if newLeaves == leaves:
+            break
+        else:
+            leaves = newLeaves
+    return tree
 
-def getFrame(cmd, cursor):
-    cursor.execute(cmd)
-    raw = cursor.fetchall()
-    columns = [column[0] for column in cursor.description]
-    dictArr = []
-    for row in raw:
-        entry = {}
-        for col in columns:
-            entry[columns.index(col)] = row[columns.index(col)]
-        dictArr.append(entry)
-    frame = pd.DataFrame(dictArr)
-    if len(frame.columns) == 0:
-        for col in columns:
-            frame[col] = [None]
-    else:
-        frame.columns = columns
-    return frame
+def getData(trees, features, ti, cursor):
+    newTrees = []
+    for t in trees:
+        newTrees.append(t.getAncestorTree(features))
+    bestTree = trees[0]  #decide which tree is best if one exists
+    cursor.execute(bestTree.writeSQL)
+    #transform into dataframe and return
 
-def createOneTable(ti, cursor):
-    roots = getRootParents(ti)
-    cmds = {}
-    frames = {}
-    for r in roots:
-        cmds[r] = createComd(r, ti)
-        frames[r] = getFrame(cmds[r], cursor)
-    return frames
+def getValidDescriptions():
+    namesFrame = ED.getValidNamesFrame(connect(), getTableNames())
+    names = []
+    for i in range(len(namesFrame.index)):
+        names.append(namesFrame["feat_description"][i])
+    return names
 
-def printTable(name, ti):
-    print("Name: " + name + " PK: " + str(ti[1][name]) + " FKs: " +
-          str(ti[2][name]))
+
+
+
+
+
+
+
+
+
+
+    def createComd(root, ti):
+        included = []
+        included.append(root)
+        cmd = "SELECT * FROM " + root
+        #lists of children indexed by parents
+        children = {}
+        children[root] = getChildren(root, ti)
+        numChildren = len(children[root])
+        while numChildren > 0:    # No no no lol
+            newChildren = {}
+            for p in children:
+                for c in children[p]:
+                    if c not in included:
+                        included.append(c)
+                        newChildren[c] = getChildren(c, ti)
+                        # Potential problem with multi column keys
+                        cmd += "\nFULL JOIN " + c + "\nON " + c + "." + ti[1][c][0] + \
+                               "=" + p + "." + ti[1][c][0]
+            children = newChildren
+            numChildren = 0
+            for p in children:
+                numChildren += len(children[p])
+        return cmd
+
+    def getFrame(cmd, cursor):
+        cursor.execute(cmd)
+        raw = cursor.fetchall()
+        columns = [column[0] for column in cursor.description]
+        dictArr = []
+        for row in raw:
+            entry = {}
+            for col in columns:
+                entry[columns.index(col)] = row[columns.index(col)]
+            dictArr.append(entry)
+        frame = pd.DataFrame(dictArr)
+        if len(frame.columns) == 0:
+            for col in columns:
+                frame[col] = [None]
+        else:
+            frame.columns = columns
+        return frame
+
+    def createOneTable(ti, cursor):
+        roots = getRootParents(ti)
+        cmds = {}
+        frames = {}
+        for r in roots:
+            cmds[r] = createComd(r, ti)
+            #frames[r] = getFrame(cmds[r], cursor)
+        return cmds
+
+    def printTable(name, ti):
+        print("Name: " + name + " PK: " + str(ti[1][name]) + " FKs: " +
+              str(ti[2][name]))
 
 ########## main ##########
 if __name__ == "__main__":
@@ -143,5 +237,8 @@ if __name__ == "__main__":
     ti[0] = tableNames
     ti[1] = PKs
     ti[2] = FKs
-    print(createOneTable(ti, cursor))
+    descs = getValidDescriptions()
+    for d in descs:
+        print(d)
+    print(len(descs))
 
--- DBPreprocessing/EnumerateDescriptions.py
+++ DBPreprocessing/EnumerateDescriptions.py
@@ -1,12 +1,4 @@
-#Indexed in dictionary by table name:
-#    All features in the table
-#    All paths from table to the origin
-#    
-#The common index for two or more tables will be the primary key of the first common ancestor between all tables
-
-import pyodbc
 import pandas as pd
-from DBPMain import getTableNames
 
 SQLCODEHEAD = "SELECT [class_name], [attr_name], [attr_descript] " + \
                 "FROM [dbo].[DICTIONARY] " + \
@@ -19,25 +11,32 @@ SQLCODETAIL = "AND attr_descript IS NOT NULL " + \
                   "AND attr_descript NOT LIKE 'Name of user who updated row' " + \
                   "AND attr_descript NOT LIKE 'Autotrak specific data'"        
 
-cnxn = pyodbc.connect("DRIVER={SQL Server}; SERVER=MYPC\SQLEXPRESS; DATABASE=BHBackupRestore; UID = SQLDummy; PWD = bushdid9/11")
-cursor = cnxn.cursor()
-tableNames = getTableNames()
-SQLCODE = SQLCODEHEAD
-for name in tableNames:
-    if tableNames.index(name) != (len(tableNames) - 1):    
-        SQLCODE += "class_name = '" + name + "' OR \n" 
-    else:
-        SQLCODE += "class_name = '" + name + "') "
-SQLCODE += SQLCODETAIL
-print(SQLCODE)      
-cursor.execute(SQLCODE)
-table = cursor.fetchall()
-columns = [column[0] for column in cursor.description]
-dictArr = []
-for row in table:
-    entry = {}
-    for col in columns:
-        entry[columns.index(col)] = row[columns.index(col)]
-        dictArr.append(entry)
-frame = pd.DataFrame(dictArr)
-print(frame)
+def getValidNamesFrame(cursor, tableNames):
+    SQLCODE = SQLCODEHEAD
+    for name in tableNames:
+        if tableNames.index(name) != (len(tableNames) - 1):
+            SQLCODE += "class_name = '" + name + "' OR \n"
+        else:
+            SQLCODE += "class_name = '" + name + "') "
+    SQLCODE += SQLCODETAIL
+    cursor.execute(SQLCODE)
+    table = cursor.fetchall()
+    cols = [column[0] for column in cursor.description]
+    dict = []
+    toDrop = []
+    for col in cols:
+        entry = []
+        for row in table:
+            if row[cols.index(col)] in entry and col == 'attr_descript':
+                toDrop.append(table.index(row))
+            entry.append(row[cols.index(col)])
+        dict.append(entry.copy())
+    frame = pd.DataFrame(dict)
+    frame = frame.T
+    cols = ["table_name", "feature_name", "feat_description"]
+    frame.columns = cols
+    frame = frame.drop(0)
+    for d in toDrop:
+        frame = frame.drop(d)
+    frame = frame.reset_index()
+    return frame
