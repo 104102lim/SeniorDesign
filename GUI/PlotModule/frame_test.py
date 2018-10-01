fb23e36b8394de1e5183f07e59809ebb37d6d6d6
xxuil <xxuil412159@gmail.com>
2018-09-30 23:35:32 -0500

    Plotting module

--- GUI/PlotModule/frame_test.py
+++ GUI/PlotModule/frame_test.py
@@ -1,142 +1,128 @@
 # -*- coding: utf-8 -*-
 """
 ---------------------------------------------------------------
-    frame.py
-    
-    GUI Module
-    
-    @author: Eric Ramos
+frame_test
 ---------------------------------------------------------------
 """
 
+from __future__ import unicode_literals
 import sys
-import pandas as pd
-from PyQt5.QtWidgets import QComboBox, QLineEdit, QMainWindow, QLabel, QApplication, QPushButton,QCheckBox
-from PyQt5.QtWidgets import QScrollArea, QTableWidget,QVBoxLayout, QTableWidgetItem, QWidget
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
- 
-class App(QMainWindow):
- 
-    def __init__(self):
-        #super().__init__()
-        super(App, self).__init__()
-        self.title = 'Analysis Toolkit Prototype'
-        self.left = 500
-        self.top = 100
-        self.width = 800
-        self.height = 800
-        self.initUI()
- 
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
- 
-####### LINEAR REGRESSION #######
-        label = QLabel('Linear Regression', self)
-        label.move(20,490)
-        label.resize(250,50)
-        featureX = QComboBox(self)
-        featureX.setToolTip('Select feature for X axis')
-        featureX.addItem("Option 1")
-        featureX.addItem("Option 2")
-        featureX.move(150, 500)
-        featureY = QComboBox(self)
-        featureY.setToolTip('Select feature for Y axis')
-        featureY.addItem("Option 1")
-        featureY.addItem("Option 2")
-        featureY.move(300, 500)
-        yIntercept = QCheckBox("Y-Intercept",self)
-        yIntercept.move(450, 500)
-        rSquared = QCheckBox("R^2",self)
-        rSquared.move(550, 500)
-        slopeCheck = QCheckBox("Slope",self)
-        slopeCheck.move(600, 500) 
-        plotButton = QPushButton('Plot', self)
-        plotButton.setToolTip('Use button to plot linear regression')
-        plotButton.clicked.connect(self.on_click)
-        plotButton.move(700,500)
-        plotButton.resize(50,50)
-        
-####### FILTERING #######
-        feature1 = QComboBox(self)
-        feature1.setToolTip('Select feature 1')
-        feature1.addItem("Option 1")
-        feature1.addItem("Option 2")
-        feature1.move(50, 600)
-        whereLabel = QLabel('WHERE', self)
-        whereLabel.move(152,590)
-        whereLabel.resize(250,50)
-        feature2 = QComboBox(self)
-        feature2.setToolTip('Select feature 2')
-        feature2.addItem("Option 1")
-        feature2.addItem("Option 2")
-        feature2.move(200, 600)
-        isLabel = QLabel('IS', self)
-        isLabel.move(350,590)
-        isLabel.resize(250,50)
-        logic = QComboBox(self)
-        logic.setToolTip('Select logic')
-        logic.addItem("<")
-        logic.addItem(">")
-        logic.move(400, 600)
-        logic.resize(50,25)
-        self.threshold = QLineEdit(self)
-        self.threshold.setToolTip('Input numeric value')
-        self.threshold.move(500,600)
-        self.threshold.resize(100,25)
-        filterButton = QPushButton('Filter', self) 
-        filterButton.setToolTip('Use button to filter the feature based on the chosen logic')
-        filterButton.clicked.connect(self.display_input)
-        filterButton.move(700,600)
-        filterButton.resize(50,50)
-        
-####### RAW OUTPUT DATA SAMPLE #######
-        #self.scroll = QScrollArea(self)
-        #self.layout = QVBoxLayout(self)
-        self.table = QTableWidget(self)
-        #self.scroll.setWidget(self.table)
-        #self.layout.addWidget(self.table)  
-        self.table.move(550,50)
-        self.table.resize(200,400)
-        df = pd.DataFrame({"a" : [4 ,5, 6],"b" : [7, 8, 9],"c" : [10, 11, 12]},index = [1, 2, 3])
-        self.table.setColumnCount(len(df.columns))
-        self.table.setRowCount(len(df.index))
-        for i in range(len(df.index)):
-            for j in range(len(df.columns)):
-                self.table.setItem(i,j,QTableWidgetItem(str(df.iloc[i, j])))
-                
-####### FILE OPTIONS ####### 
-        saveButton = QPushButton('Save', self) 
-        saveButton.setToolTip('Use button to save the query')
-        #saveButton.clicked.connect(self.save)
-        saveButton.move(250,700)
-        saveButton.resize(100,50)
-        loadButton = QPushButton('Load', self) 
-        loadButton.setToolTip('Use button to load a query')
-        #loadButton.clicked.connect(self.load)
-        loadButton.move(350,700)
-        loadButton.resize(100,50)
-        exportButton = QPushButton('Export', self) 
-        exportButton.setToolTip('Use button to export a query')
-        #exportButton.clicked.connect(self.export)
-        exportButton.move(450,700)
-        exportButton.resize(100,50)
+import os
+import random
+import matplotlib
+# Make sure that we are using QT5
+matplotlib.use('Qt5Agg')
+# Uncomment this line before running, it breaks sphinx-gallery builds
+from PyQt5 import QtCore, QtWidgets
 
-### ACTION METHODS ###
-    @pyqtSlot()    
-    def on_click(self):
-        print('Plot graph')
-        label2 = QLabel('Linear Regression', self)
-        label2.move(20,100)
-    def display_input(self):
-        textboxValue = self.threshold.text();
-        print("You typed" + " " + textboxValue)
-        
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+
+progname = os.path.basename(sys.argv[0])
+progversion = "0.1"
+
+
+class MyMplCanvas(FigureCanvas):
+    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+
+        self.compute_initial_figure()
+
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QtWidgets.QSizePolicy.Expanding,
+                                   QtWidgets.QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+    def compute_initial_figure(self):
+        pass
+
+
+class MyStaticMplCanvas(MyMplCanvas):
+    """Simple canvas with a sine plot."""
+    def compute_initial_figure(self):
+        t = arange(0.0, 3.0, 0.01)
+        s = sin(2*pi*t)
+        self.axes.plot(t, s)
+        self.axes.set_xlabel(xlabel="XXX")
+        self.axes.set_ylabel(ylabel="YYY")
+
+    def update_figure(self, dataX, dataY, Xlabel, Ylabel):
+        # Build a list of 4 random integers between 0 and 10 (both inclusive)
+        l = random.randint(0, 10)
+
+        dataX = dataX * l
+        dataY = dataY * l
+
+        self.axes.cla()
+        self.axes.plot(dataX, dataY)
+        self.axes.set_xlabel(xlabel=Xlabel)
+        self.axes.set_ylabel(ylabel=Ylabel)
+        self.draw()
+
+
+class ApplicationWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        t = arange(0.0, 3.0, 0.01)
+        s = sin(2 * pi * t)
+
+        QtWidgets.QMainWindow.__init__(self)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.setWindowTitle("application main window")
+
+        self.file_menu = QtWidgets.QMenu('&File', self)
+        self.file_menu.addAction('&Import', self.fileQuit,
+                                 QtCore.Qt.CTRL + QtCore.Qt.Key_I)
+        self.file_menu.addAction('&Quit', self.fileQuit,
+                                 QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
+        self.menuBar().addMenu(self.file_menu)
+
+        self.help_menu = QtWidgets.QMenu('&Help', self)
+        self.menuBar().addSeparator()
+        self.menuBar().addMenu(self.help_menu)
+
+        self.help_menu.addAction('&About', self.about)
+
+        self.main_widget = QtWidgets.QWidget(self)
+
+        # Canvas Setup
+        layout = QtWidgets.QVBoxLayout(self.main_widget)
+        sc = MyStaticMplCanvas(self.main_widget, width=5, height=4, dpi=100)
+        # Plot Button
+        pbutton = QtWidgets.QPushButton('Plot')
+        pbutton.clicked.connect(lambda: sc.update_figure(t, s, "XXX", "YYY"))
+        # Toolbar Setup
+        toolBar = NavigationToolbar(sc, self)
+
+        layout.addWidget(sc)
+        layout.addWidget(toolBar)
+        layout.addWidget(pbutton)
+
+        self.main_widget.setFocus()
+        self.setCentralWidget(self.main_widget)
+
+        self.statusBar().showMessage("Testing", 2000)
+
+    def fileQuit(self):
+        self.close()
+
+    def closeEvent(self, ce):
+        self.fileQuit()
+
+    def about(self):
+        QtWidgets.QMessageBox.about(self, "About", """Senior Design GUI prototype""")
+
+
 if __name__ == '__main__':
-    app = 0
-    app = QApplication(sys.argv)
-    main = App()
-    main.show()
-    #main.showMaximized()
-    sys.exit(app.exec_())
+    qApp = QtWidgets.QApplication(sys.argv)
+    aw = ApplicationWindow()
+    aw.setWindowTitle("%s" % progname)
+    aw.show()
+    sys.exit(qApp.exec_())
