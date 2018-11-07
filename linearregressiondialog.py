942862ebeb11be617009f98197a3f956ea5f249b
Joey Gallardo <joeygallardo22@utexas.edu>
2018-11-07 14:11:19 -0600

    linearRegressionDialog function comboboxes resized

--- linearregressiondialog.py
+++ linearregressiondialog.py
@@ -5,13 +5,14 @@ from PyQt5.QtWidgets import QComboBox, QLabel, QPushButton, QCheckBox, QComplete
 from DatabasePreprocessing import getDescriptions
 
 class linearRegressionDialog(QtWidgets.QMainWindow):
-    def __init__(self, parent=None):
-        super(linearRegressionDialog, self).__init__(parent)
+       def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
         self.title = 'Linear Regression'
-        self.left = 500
+        self.left = 50
         self.top = 100
-        self.width = 600
-        self.height = 100
+        self.width = 1200
+        self.height = 120
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.setFixedSize(self.size())
@@ -26,19 +27,21 @@ class linearRegressionDialog(QtWidgets.QMainWindow):
         
         self.featureX = QComboBox(self)
         self.featureX.setToolTip('Select feature for X axis')
-        self.featureX.move(50, 20)
-        self.featureX.resize(100, 20)
+        self.featureX.move(15, 14)
+        self.featureX.resize(840, 20)
         label = QLabel('X-Axis Feature', self)
-        label.move(50, 30)
+        label.move(18, 20)
         label.resize(100, 50)
         self.featureY = QComboBox(self)
         self.featureY.setToolTip('Select feature for Y axis')
-        self.featureY.move(200, 20)
-        self.featureY.resize(100, 20)
+        self.featureY.move(15, 60)
+        self.featureY.resize(840, 20)
         label = QLabel('Y-Axis Feature', self)
-        label.move(200, 30)
+        label.move(20, 62)
         label.resize(100, 50)
         descriptions = getDescriptions()
+        descriptions = [d.lower() for d in descriptions]
+        descriptions.sort()
         self.featureX.setInsertPolicy(QComboBox.NoInsert)
         self.featureX.setEditable(True)
         self.featureX.setCompleter(QCompleter(descriptions))
@@ -48,13 +51,13 @@ class linearRegressionDialog(QtWidgets.QMainWindow):
         self.featureY.setCompleter(QCompleter(descriptions))
         self.featureY.completer().setCompletionMode(QCompleter.UnfilteredPopupCompletion)
         self.yIntercept = QCheckBox("Y-Intercept", self)
-        self.yIntercept.move(350, 10)
+        self.yIntercept.move(880, 10)
         self.rSquared = QCheckBox("R^2", self)
-        self.rSquared.move(350, 40)
+        self.rSquared.move(880, 40)
         self.slopeCheck = QCheckBox("Slope", self)
-        self.slopeCheck.move(350, 70)
+        self.slopeCheck.move(880, 70)
         plotButton = QPushButton('Plot', self)
         plotButton.setToolTip('Use button to plot linear regression')
         plotButton.clicked.connect(self.parent().plotLinearRegression)
-        plotButton.move(500, 20)
-        plotButton.resize(50, 50)
+        plotButton.move(1020, 18)
+        plotButton.resize(90, 90)
